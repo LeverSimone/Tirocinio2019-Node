b13fe799461f0477e80212005bf5be998d426b65
Simone Lever <simonelever@gmail.com>
2019-02-18 22:44:50 +0100

    It recognised if it is running on Heroku or locally
    fix for Heroku

--- global_settings.js
+++ global_settings.js
@@ -2,9 +2,7 @@ const PORT = process.env.PORT || 3000;
 const DESTINATION_PORT_RASA = 5000;
 const DESTINATION_PORT_PUPPETEER = 4000;
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost:' + PORT;
-//const DESTINATION_URL_RASA = 'http://localhost:' + DESTINATION_PORT_RASA;
-const DESTINATION_URL_RASA = "https://tirocinio2019-rasanlu.herokuapp.com";
-const DESTINATION_URL_PUPPETEER = "https://tirocinio2019-puppeteer.herokuapp.com";
-//const DESTINATION_URL_PUPPETEER = "http://localhost:" + DESTINATION_PORT_PUPPETEER;
+const DESTINATION_URL_RASA = process.env.HEROKU ? "https://tirocinio2019-rasanlu.herokuapp.com" : 'http://localhost:' + DESTINATION_PORT_RASA;
+const DESTINATION_URL_PUPPETEER = process.env.HEROKU ? "https://tirocinio2019-puppeteer.herokuapp.com" : "http://localhost:" + DESTINATION_PORT_PUPPETEER;
 
 module.exports = {PORT, DESTINATION_PORT_RASA, DESTINATION_PORT_PUPPETEER, SERVER_URL, DESTINATION_URL_RASA, DESTINATION_URL_PUPPETEER};
