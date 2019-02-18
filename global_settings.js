1c91d6cb826b6c7b2a96dcf9b70f6d23ec029c74
Simone Lever <simonelever@gmail.com>
2019-02-18 12:15:37 +0100

    Node can manage intent:list with a resource

--- global_settings.js
+++ global_settings.js
@@ -2,8 +2,8 @@ const PORT = process.env.PORT || 3000;
 const DESTINATION_PORT_RASA = 5000;
 const DESTINATION_PORT_PUPPETEER = 4000;
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost:' + PORT;
-//const DESTINATION_URL_RASA = 'http://localhost:' + DESTINATION_PORT_RASA;
-const DESTINATION_URL_RASA = "https://tirocinio2019-rasanlu.herokuapp.com";
+const DESTINATION_URL_RASA = 'http://localhost:' + DESTINATION_PORT_RASA;
+//const DESTINATION_URL_RASA = "https://tirocinio2019-rasanlu.herokuapp.com";
 //const DESTINATION_URL_PUPPETEER = "https://tirocinio2019-rasanlu.herokuapp.com";
 const DESTINATION_URL_PUPPETEER = "http://localhost:" + DESTINATION_PORT_PUPPETEER;
 
--- index.js
+++ index.js
@@ -22,48 +22,56 @@ app.post('/conversation', async (req, res) => {
         //L'azione inserita è un sito
         if (body.action.includes('http')) {
             let objToPuppetteer = { site: body.action };
-            let resultToSend = { action: "Sito aperto: " + body.action };
+            let resultToSend = { action: "Site opened: " + body.action };
             try {
                 let result = await MY_FUNCTIONS.post(objToPuppetteer, GLOBAL_SETTINGS.DESTINATION_URL_PUPPETEER + "/opensite", 'application/json');
                 structureBotify = await result.json();
 
-                console.log(structureBotify);
+                //console.log(structureBotify);
 
                 res.json(resultToSend);
             } catch (error) {
                 res.status(500).send(error);
             }
         }
-        // è un'altro tipo di azione, chiedo a Rasa
+        //è un'altro tipo di azione, chiedo a Rasa
         else {
 
-            let object = { q: body.action, project: "helloname" };
+            let object = { q: body.action, project: "conversation" };
             try {
-                let result = await MY_FUNCTIONS.post(object, GLOBAL_SETTINGS.DESTINATION_URL_RASA + "/parse", 'application/x-yml');
-                let resultjson = await result.json();
-                let resToSend = { intent: resultjson.intent.name }
-                if (resultjson.entities[0])
-                    resToSend.entity = resultjson.entities[0].value;
-                
-                console.log(structureBotify);
-                console.log(resToSend);
+                let reqRasa = await MY_FUNCTIONS.post(object, GLOBAL_SETTINGS.DESTINATION_URL_RASA + "/parse", 'application/x-yml');
+                let resRasa = await reqRasa.json();
+                let intentRasa = { intent: resRasa.intent.name }
+                if (resRasa.entities[0])
+                    intentRasa.entity = resRasa.entities[0].value;
+
+                //console.log(structureBotify);
+                //console.log(intentRasa);
 
                 //confronto intent e attribute di Rasa con quelli imparati da Node
-                if(structureBotify.intent == resToSend.intent)
-                {
-                    if(structureBotify.resource == resToSend.entity)
-                    {
-                        console.log("uguale");
+                if (structureBotify) {
+                    if (structureBotify.intent == intentRasa.intent) {
+                        if (structureBotify.resource == intentRasa.entity) {
+                            res.json({ action: "Intent recognised: " + structureBotify.intent + ", resource: " + structureBotify.resource + ", attribute: none" });
+                        }
+                        else {
+                            res.json({ action: "Sorry, there are no " + intentRasa.entity + " in this site. Did you mean " + structureBotify.resource + "?" });
+                        }
                     }
+                    else {
+                        res.json({ action: "Intent not recognised, in this site you can: " + structureBotify.intent });
+                    }
+                }
+                else {
+                    res.json({ action: "You have to open a site before doing an action" });
                 }
             } catch (error) {
                 res.status(500).send(error);
             }
-
         }
     }
     else {
-        res.sendStatus(400);
+        res.status(400).send('Action is empty');
     }
 })
 
