0d7b6a5c495e96e53dc50a25dc5ff9696828302e
Simone Lever <simonelever@gmail.com>
2019-02-22 11:35:32 +0100

    Node manage multiple comp/res and remember context
    It works with Rasa NLU components
    Now it can manage multiple components/resources

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}));
 
 const GLOBAL_SETTINGS = require("./global_settings.js");
 const MY_FUNCTIONS = require("./function.js");
-
-var structureBotify = null;
 
 app.use('/', express.static('public'));
 
@@ -21,54 +28,38 @@ app.post('/conversation', async (req, res) => {
     if (body.action) {
         //L'azione inserita è un sito
         if (body.action.includes('http')) {
-            let objToPuppetteer = { site: body.action };
-            let resultToSend = { action: "Site opened: " + body.action };
-            try {
-                let result = await MY_FUNCTIONS.post(objToPuppetteer, GLOBAL_SETTINGS.DESTINATION_URL_PUPPETEER + "/opensite", 'application/json');
-                structureBotify = await result.json();
-
-                console.log(structureBotify);
-
+            let resultToSend = await MY_FUNCTIONS.openSite(req, body.action);
+            if(resultToSend.error)
+            {
+                res.status(500).send(resultToSend.error);
+            } else {
                 res.json(resultToSend);
-            } catch (error) {
-                console.log(error)
-                res.status(500).send(error);
             }
         }
         //è un'altro tipo di azione, chiedo a Rasa
-        else {
-
-            let object = { q: body.action, project: "conversation" };
-            try {
-                let reqRasa = await MY_FUNCTIONS.post(object, GLOBAL_SETTINGS.DESTINATION_URL_RASA + "/parse", 'application/x-yml');
-                let resRasa = await reqRasa.json();
-                let intentRasa = { intent: resRasa.intent.name }
-                if (resRasa.entities[0])
-                    intentRasa.entity = resRasa.entities[0].value;
-
-                //console.log(structureBotify);
+        else if (req.session.site) {
+            let intentRasa = await MY_FUNCTIONS.askRasa(body.action);
+            if (intentRasa.error) {
+                res.status(500).send(intentRasa.error);
+            }
+            else {
+                //console.log("intentRasa:")
                 //console.log(intentRasa);
 
-                //confronto intent e attribute di Rasa con quelli imparati da Node
-                if (structureBotify) {
-                    if (structureBotify.intent == intentRasa.intent) {
-                        if (structureBotify.resource == intentRasa.entity) {
-                            res.json({ action: "Intent recognised: " + structureBotify.intent + ", resource: " + structureBotify.resource + ", attribute: none" });
-                        }
-                        else {
-                            res.json({ action: "Sorry, there are no " + intentRasa.entity + " in this site. Did you mean " + structureBotify.resource + "?" });
-                        }
-                    }
-                    else {
-                        res.json({ action: "Intent not recognised, in this site you can: " + structureBotify.intent });
-                    }
+                //prendo/setto resource in sessione
+                if (!intentRasa.resource) { 
+                    intentRasa.resource = req.session.context;
+                } else {
+                    req.session.context = intentRasa.resource;
                 }
-                else {
-                    res.json({ action: "You have to open a site before doing an action" });
-                }
-            } catch (error) {
-                res.status(500).send(error);
+
+                responseToSend = MY_FUNCTIONS.validator(req.session.site.structure, intentRasa, req);
+                //console.log(responseToSend);
+                res.json({ action: responseToSend });
             }
+        }
+        else {
+            res.json({ action: "You have to open a site before doing an action" });
         }
     }
     else {
