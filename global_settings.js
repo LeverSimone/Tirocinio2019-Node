8c5526fab69799d4a6438b0e1e09133e19b01537
Simone Lever <simonelever@gmail.com>
2019-05-13 17:40:02 +0200

    added /sendMessage

--- global_settings.js
+++ global_settings.js
@@ -4,6 +4,6 @@ const DESTINATION_PORT_PUPPETEER = 4000;
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost:' + PORT;
 const DESTINATION_URL_RASA = process.env.HEROKU ? "https://tirocinio2019-rasanlu.herokuapp.com" : 'http://localhost:' + DESTINATION_PORT_RASA;
 const DESTINATION_URL_PUPPETEER = process.env.HEROKU ? "https://tirocinio2019-puppeteer.herokuapp.com" : "http://localhost:" + DESTINATION_PORT_PUPPETEER;
-const TELEGRAM_BOT_URL = process.env.BOT_TOKEN ? "https://api.telegram.org/bot"+process.env.BOT_TOKEN : null ; 
+const TELEGRAM_BOT_URL = process.env.BOT_TOKEN ? "https://api.telegram.org/bot"+process.env.BOT_TOKEN+'/sendMessage' : null ; 
 
 module.exports = {PORT, SERVER_URL, DESTINATION_URL_RASA, DESTINATION_URL_PUPPETEER, TELEGRAM_BOT_URL};
