50a911c1166ececadb150940435577bf92f94852
Simone Lever <simonelever@gmail.com>
2019-05-15 00:32:14 +0200

    C:/Program Files/Git/start message manage for Telegram

--- function.js
+++ function.js
@@ -78,53 +78,6 @@ async function askToValide(comand, configurationURI) {
     }
 }
 
-function objToRun(validation, link) {
-    if (!validation.matching_failed[0] && validation.matching[0]) {
-        let object = {
-            url: link,
-            component: validation.matching[0].match.component,
-            query: {
-                intent: validation.intent.name,
-                resource: {
-                    name: null,
-                    selector: null,
-                    param_attr: {
-                        name: null,
-                        selector: null
-                    },
-                    operation: null,
-                    attributes: []
-                }
-            }
-        };
-        //prendo operation
-        validation.entities.forEach(entities => {
-            if(entities.entity.includes("op")) {    
-                object.query.resource.operation = entities.value;
-            }
-        });
-        //prendo resource e attributes dall'oggetto
-        validation.matching.forEach(entities => {
-            //console.log("entities");
-            //console.log(entities);
-            if (entities.entity.entity == "resource") {
-                object.query.resource.name = entities.match.resource;
-                object.query.resource.selector = entities.match.selector;
-                //inserisco tutti gli attributes compatibili con la risorsa inserita
-                entities.match.attributes.forEach(attributes => {
-                    object.query.resource.attributes.push({name: attributes.name, selector: attributes.selector});
-                });
-            } //inserisco gli attributes indicati dall'utente
-            else if (entities.entity.entity == "attribute") {
-                object.query.resource.param_attr = entities.match;
-            }
-        });
-        return object;
-    }
-    else
-        return false;
-}
-
 function newObjToRun(validation, link) {
     if (!validation.matching_failed[0] && validation.matching[0]) {
         let object = {
@@ -166,62 +119,4 @@ function newObjToRun(validation, link) {
         return false;
 }
 
-function validator(validation) {
-    found = { match: { intent: validation.intent.name, resources: [], attributes: [] }, not_matched: { resources: [], attributes: [] } }
-    //metti in found un oggetto di tipo resources/attributes e il suo valore
-    validation.matching.forEach(entities => {
-        if (entities.relation == 'equal') {
-            entities.entity.entity == 'resource' ? found.match.resources.push(entities.entity.value) : found.match.attributes.push(entities.entity.value);
-        }
-        else if (entities.relation == 'syn') {
-            entities.entity.entity == 'resource' ? found.match.resources.push(entities.match.resource) : found.match.attributes.push(entities.match);
-        }
-        else {
-            let object = { relation: entities.relation, insert: entities.entity.value };
-            if (entities.entity.entity == 'resource') {
-                object.found = entities.match.resource;
-                found.match.resources.push(object);
-            } else {
-                object.found = entities.match;
-                found.match.attributes.push(object);
-            }
-        }
-    });
-    validation.matching_failed.forEach(entities => {
-        entities.entity == 'resource' ? found.not_matched.resources.push(entities.value) : found.not_matched.attributes.push(entities.value);
-    });
-
-    return found;
-}
-
-function composeResult(objectValidated) {
-    let resultToSend = "";
-    if (objectValidated.match) {
-        resultToSend = "Intent recognised: " + objectValidated.match.intent;
-        resultToSend = writeResult(resultToSend, objectValidated.match.resources, 'resource', true)
-        resultToSend = writeResult(resultToSend, objectValidated.match.attributes, 'attribute', true)
-    }
-    if (objectValidated.not_matched) {
-        resultToSend = writeResult(resultToSend, objectValidated.not_matched.resources, 'resource', false)
-        resultToSend = writeResult(resultToSend, objectValidated.not_matched.attributes, 'attribute', false)
-    }
-    return resultToSend;
-}
-
-function writeResult(resultToSend, object, resAtt, match) {
-    object.forEach(element => {
-        if (element.relation) {
-            if (match)
-                resultToSend += ", " + resAtt + " insert: " + element.insert + ", " + resAtt + " relation: " + element.relation + ", " + resAtt + " found: " + element.found;
-        } else {
-            if (match) {
-                resultToSend += ", " + resAtt + ": " + element;
-            } else {
-                resultToSend += ", " + resAtt + " not found: " + element;
-            }
-        }
-    });
-    return resultToSend;
-}
-
-module.exports = {post, configureValidator, openSite, askToValide, validator, composeResult, takeConfID, objToRun, newObjToRun};
+module.exports = {post, configureValidator, openSite, askToValide, takeConfID, newObjToRun};
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(session({
 const GLOBAL_SETTINGS = require("./global_settings.js");
 const MY_FUNCTIONS = require("./function.js");
 
-//Array in mempory per mantenere l'associazione tra la chat e il sito di cui si sta parlando
+//Array in memory per mantenere l'associazione tra la chat e il sito di cui si sta parlando
 var siteTelegram = [];
 
 app.use('/', express.static('public'));
@@ -29,8 +29,9 @@ app.get('/', (req, res) => {
     res.json({ status: 'ok' });
 })
 
-function setSession (chatId, valueToSet, req) {
-    if(chatId) {
+//Settare contenuto per la sessione API standard e per l'array in memory per Telegram
+function setSession(chatId, valueToSet, req) {
+    if (chatId) {
         siteTelegram[chatId] = valueToSet;
     } else {
         req.session.configurationURI = valueToSet;
@@ -45,24 +46,19 @@ async function conversation(body, req, chatId) {
 
             //resetto session context
             //req.session.context = undefined;
-            //setSession(chatId, undefined, req);
-            
+
             let configurationURI = await MY_FUNCTIONS.takeConfID(body.action);
             if (configurationURI.error) {
-                //res.status(500).send(configurationURI.error);
                 return { action: configurationURI.error, error: 500 };
             }
             else if (configurationURI.id) {
-                //req.session.configurationURI = configurationURI.id;
                 setSession(chatId, configurationURI.id, req);
-                //res.json(resultToSend);
                 return resultToSend;
             } else {
 
                 //impariamo la struttura del sito da Botify
                 let structureBotify = await MY_FUNCTIONS.openSite(body.action);
                 if (structureBotify.error) {
-                    //res.status(500).send(structureBotify.error);
                     return { action: structureBotify.error, error: 500 };
                 } else {
                     //inserisco il link del sito nella struttura imparata
@@ -71,18 +67,15 @@ async function conversation(body, req, chatId) {
                     //configuriamo Rasa per sapere la struttura del sito in cui ci troviamo
                     configurationURI = await MY_FUNCTIONS.configureValidator(structureBotify);
                     if (configurationURI.error) {
-                        //res.status(500).send(configurationURI.error);
                         return { action: configurationURI.error, error: 500 };
                     }
                     else {
                         //salvo in sessione configurationURI
-                        //req.session.configurationURI = configurationURI.id;
                         setSession(chatId, configurationURI.id, req);
 
                         //Debugging Frontend
                         resultToSend.log = JSON.stringify(structureBotify, null, " ");
 
-                        //res.json(resultToSend);
                         return resultToSend;
                     }
                 }
@@ -91,28 +84,16 @@ async function conversation(body, req, chatId) {
         //è un'altro tipo di azione, un comando, ex: "list me proposals", chiamo Rasa per fare la validazione
         else if (req.session.configurationURI || siteTelegram[chatId]) {
 
-            let configurationURI = req.session.configurationURI ? req.session.configurationURI : siteTelegram[chatId] ; 
+            let configurationURI = req.session.configurationURI ? req.session.configurationURI : siteTelegram[chatId];
 
             //chiamo il server Rasa per fare la validazione dell'input utente
             let validation = await MY_FUNCTIONS.askToValide(body.action, configurationURI);
 
             if (validation.error) {
-                //res.status(500).send(validation.error);
                 return { action: validation.error, error: 500 };
             }
             else {
-                /*console.log("\n");
-                console.log(validation);
-                if (validation.matching[0]) {
-                    console.log("validation.matching[0].entity");
-                    console.log(validation.matching[0].entity);
-                    console.log("validation.matching[0].match");
-                    console.log(validation.matching[0].match);
-                }
-                console.log("\n");*/
-
                 let objToEngine = MY_FUNCTIONS.newObjToRun(validation, configurationURI);
-                //console.log(objToEngine);
 
                 //inserisci da context, da implementare nella parte di Python
                 /*
@@ -128,25 +109,21 @@ async function conversation(body, req, chatId) {
                     objToEngine = { action: "You insert something that is not in the site" }
                     //Debugging Frontend
                     objToEngine.log = JSON.stringify(validation, null, " ");
-                    //res.json(objToEngine)
                     return objToEngine;
                 } else {
                     //Debugging Frontend
                     objToEngine.log = JSON.stringify(objToEngine, null, " ");
                     let result = await engine.processIntent(objToEngine);
                     objToEngine.action = JSON.stringify(result, null, " ");
-                    //res.json(objToEngine);
                     return objToEngine;
                 }
             }
         }
         else {
-            //res.json({ action: "You have to open a site before doing an action" });
             return { action: "You have to open a site before doing an action" };
         }
     }
     else {
-        //res.status(400).send('Action is empty');
         return { action: "Action is empty'", error: 400 };
     }
 }
@@ -157,34 +134,28 @@ app.post('/', async (req, res) => {
     const sentMessage = req.body.message.text;
     console.log(sentMessage);
 
-    let body = {action: sentMessage};
+    if (sentMessage == '/start') {
 
-    let resultToSend = await conversation(body, req, chatId);
-
-    if (resultToSend.error) {
-        res.status(resultToSend.error).send(resultToSend.action);
-    } else {
-        let object = { chat_id: chatId, text: resultToSend.action };
+        let object = { chat_id: chatId, text: 'Open a site with an URL and then write an action' };
         let responseBot = await MY_FUNCTIONS.post(object, GLOBAL_SETTINGS.TELEGRAM_BOT_URL, 'application/json');
         let responseBotJson = await responseBot.json();
-
-        console.log("response:");
-        console.log(responseBotJson);
-
         res.sendStatus(200);
+
+    } else {
+        let body = { action: sentMessage };
+        let resultToSend = await conversation(body, req, chatId);
+
+        if (resultToSend.error) {
+            res.status(resultToSend.error).send(resultToSend.action);
+        } else {
+            let object = { chat_id: chatId, text: resultToSend.action };
+            let responseBot = await MY_FUNCTIONS.post(object, GLOBAL_SETTINGS.TELEGRAM_BOT_URL, 'application/json');
+            let responseBotJson = await responseBot.json();
+            console.log("response:");
+            console.log(responseBotJson);
+            res.sendStatus(200);
+        }
     }
-    /*
-    let object = { chat_id: chatId, text: 'hello' };
-    let result = await MY_FUNCTIONS.post(object, GLOBAL_SETTINGS.TELEGRAM_BOT_URL, 'application/json');
-    let responseBot = await result.json();
-
-    console.log("response:");
-    console.log(responseBot);
-
-    res.sendStatus(200);
-    */
-    //let body = {action: sentMessage};
-    //await conversation(body, chatId);
 });
 
 app.post('/conversation', async (req, res) => {
