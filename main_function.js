07aadd3d5ce6aa5c0f19fbb826f301af90e0479a
Simone Lever <simonelever@gmail.com>
2019-08-20 14:22:41 +0200

    go site completely work

--- main_function.js
+++ main_function.js
@@ -13,37 +13,33 @@ async function conversation(body, req, chatId) {
             let resultToSend = await OPENSITE.openSite(body.action, req, chatId);
             return resultToSend;
         }
-        //è un'altro tipo di azione, un comando, ex: "list me proposals", chiamo Rasa per fare la validazione
-        else if (DATA.getURI(chatId, req)) {
-            let configurationURI = DATA.getURI(chatId, req);
-            let objectRasaURI = DATA.getRasaURI(chatId, req);
-
-            //chiamo il server Rasa per fare la validazione dell'input utente
-            let validation = await MY_FUNCTIONS.askToValide(body.action, objectRasaURI);
-
-            if (validation.error) {
-                return { action: validation.error, error: 500 };
+        //è un'altro tipo di azione, un comando, ex: "list me proposals"
+        //chiamo il server Rasa per fare la validazione dell'input utente
+        let configurationURI = DATA.getURI(chatId, req);
+        let objectRasaURI = DATA.getRasaURI(chatId, req);
+        let validation = await MY_FUNCTIONS.askToValide(body.action, objectRasaURI);
+        if (validation.error) {
+            return { action: validation.error, error: 500 };
+        }
+        if (DATA.getURI(chatId, req) || validation.intent.name == "go_site") {
+            let resultToSend;
+            // non è un'azione di tipo lista, e' show more
+            if (validation.intent && validation.intent.name == "show_more") {
+                resultToSend = LIST.show_more(chatId, req, GLOBAL_SETTINGS.NRESULT);
+            } else if (validation.intent && validation.intent.name == "article_read") {
+                resultToSend = ARTICLE.article_read();
+            } else if (validation.intent && validation.intent.name == "open_element") {
+                resultToSend = await LIST.open_element(chatId, req, validation);
+            } else if (validation.intent && validation.intent.name == "go_back") {
+                resultToSend = await COMMANDS.go_back(chatId, req);
+            } else if (validation.intent && validation.intent.name == "go_site") {
+                resultToSend = await COMMANDS.go_site(chatId, req, validation);
             }
             else {
-                let resultToSend;
-                // non è un'azione di tipo lista, e' show more
-                if (validation.intent && validation.intent.name == "show_more") {
-                    resultToSend = LIST.show_more(chatId, req, GLOBAL_SETTINGS.NRESULT);
-                } else if (validation.intent && validation.intent.name == "article_read") {
-                    resultToSend = ARTICLE.article_read();
-                } else if (validation.intent && validation.intent.name == "open_element") {
-                    resultToSend = await LIST.open_element(chatId, req, validation);
-                } else if (validation.intent && validation.intent.name == "go_back") {
-                    resultToSend = await COMMANDS.go_back(chatId, req);
-                } else if (validation.intent && validation.intent.name == "go_site") {
-                    resultToSend = await COMMANDS.go_site(chatId, req, validation);
-                }
-                else {
-                    // è un'azione di tipo lista
-                    resultToSend = await LIST.list_intent(chatId, req, validation, configurationURI, GLOBAL_SETTINGS.NRESULT);
-                }
-                return resultToSend;
+                // è un'azione di tipo lista
+                resultToSend = await LIST.list_intent(chatId, req, validation, configurationURI, GLOBAL_SETTINGS.NRESULT);
             }
+            return resultToSend;
         }
         else {
             return { action: "You have to open a site before doing an action" };
